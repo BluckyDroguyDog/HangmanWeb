d380dce46722354e3b2dd0df9a6cb5c3a0d5c038
Axelle Albert <axelle.albert@ynov.com>
2023-12-19 18:49:54 +0100

    css

--- main.go
+++ main.go
@@ -30,6 +30,14 @@ var images = []string{
 	"images/vie9.png",
 	"images/vie10.png",
 }
+var imagegif = "images/combat.gif"
+var gifs = []string{
+	"images/combat.gif",
+	"images/combatv.gif",
+	"images/combatg.gif",
+	"images/combatd.gif",
+	"images/combatp.gif",
+}
 
 type Hangman struct { // structure contenant les information a envoye au template
 	Deja       []string
@@ -37,6 +45,7 @@ type Hangman struct { // structure contenant les information a envoye au templat
 	Vie        int
 	Endmessage string
 	Imagepath  string
+	Imagegif   string
 }
 
 func Aleatoire(liste string) string {
@@ -68,12 +77,14 @@ func revealLetter(word string) string {
 	return initialWord // Retourne le mot partiellement révélé
 }
 func restart() {
+	mot = ""
 	mot = Aleatoire(listedemot)
 	print(mot) // les test iront plus vite
 	motcacher = revealLetter(mot)
 	vie = 10
 	deja = []string{}
 	endmessage = ""
+	imagegif = gifs[0]
 	start = false
 }
 
@@ -87,6 +98,15 @@ func ToUpper(s string) string {
 		result += string(h[i])
 	}
 	return result
+}
+
+func PasUtilise(l string) bool {
+	for i := 0; i < len(deja); i++ {
+		if deja[i] == l {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
@@ -106,6 +126,7 @@ func main() {
 			Vie:        vie,
 			Endmessage: endmessage,
 			Imagepath:  imagepath,
+			Imagegif:   imagegif,
 		}
 		liste := r.FormValue("liste") // choix de la liste de mots
 		if liste != "" {
@@ -117,31 +138,39 @@ func main() {
 		}
 		tLettre := false                         // Tlettre = True Lettre pour verifier si la lettre est dans le mot
 		lettre := ToUpper(r.FormValue("lettre")) //Input de la lettre mis en maj
-		if lettre != "" {
+		if lettre != "" && PasUtilise(lettre) {  //Vérifie si une lettre est envoyée et si elle n'a pas déjà été utilisée
 			deja = append(deja, lettre)
 			for i := 0; i < len(motcacher); i++ { // Parcours du mot initial
 				if mot[i] == []byte(lettre)[0] { // Vérifie si la lettre proposée est présente dans le mot
 					motcacher = motcacher[:i] + string(mot[i]) + motcacher[(i+1):] // Met à jour le mot initial avec la lettre trouvée
 					tLettre = true
+					imagegif = gifs[1]
 					if mot == motcacher {
 						endmessage = "Vous avez vaincu"
 						data.Endmessage = endmessage
+						imagegif = gifs[2]
+						data.Imagegif = imagegif
 					}
 				}
 			}
 			if tLettre == false { //perte de vie et changement d'images
 				vie--
 				imagepath = images[vie]
+				imagegif = gifs[3]
 			}
 
 		}
 		if vie == 0 { // arret de la partie
 			endmessage = "Vous avez péri(e)"
 			data.Endmessage = endmessage
+			imagegif = gifs[4]
+			data.Imagegif = imagegif
 		}
 
 		imagepath = images[vie] //Mise a jour des information de la struture
 		data.Imagepath = imagepath
+		data.Imagegif = imagegif
+		data.Endmessage = endmessage
 		data.Deja = deja
 		data.Mot = motcacher
 		data.Vie = vie
